af82f99a88fef48f511532c8c97d1d9f214fcb96
JoeydeRijk <joey.derijk@student.hu.nl>
2017-04-04 13:57:39 +0200

    gebruik Main.java en Account.java als voorbeeld, dit zijn niet de werkelijke klassen! Uitleg toegevoegd in de main

--- DJ3M_TODDS/src/authenticatie/Main.java
+++ DJ3M_TODDS/src/authenticatie/Main.java
@@ -6,16 +6,19 @@ import java.security.spec.InvalidKeySpecException;
 public class Main {
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		//het wachtwoord dat ik gebruik
 		String password = "password";
-
+		//handler
 		PasswordHandler handler = new PasswordHandler();
+		//hier maak je een 'salt' aan, deze mag gewoon publiekelijk zijn
 		byte[] salt = handler.generateSalt();
+		//encryptie van het wachtwoord
 		byte [] encryptedpassword = handler.getEncryptedPassword(password, salt);
 		Account test = new Account();
 		test.setUsername("username");
 		test.setPassword(encryptedpassword);
 		test.setSalt(salt);
-		
+		//vergelijk het meegegeven wachtwoord, met het ge-encrypte wachtwoord EN salt uit de database(in de toekomst)
 		System.out.print(handler.authenticate("password", encryptedpassword, salt));
 
 	}
